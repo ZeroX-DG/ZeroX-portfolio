b7d019afbda5aa79bf72fc36e2074bd1dc4b0341
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2017-02-21 23:23:32 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,6 +9,9 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //------------------------------------------ 
 */
+
+
+
 $("a").on('click', function(event) {
     if (this.hash !== "") {
         event.preventDefault();
@@ -22,18 +25,28 @@ $("a").on('click', function(event) {
 });
 
 $(document).ready(function(){
-    if(isInView(".btitle")){
-        $(".line").animate({
-            height: $(".lastDot").position().top - $(".dot").position().top,
-        });
-    }
+    $('html, body').animate({
+        scrollTop: 0
+    }, 1000);
 });
-
+var isAnimated = false;
 $(window).scroll(function(){
     if(isInView(".btitle")){
         $(".line").animate({
             height: $(".lastDot").position().top - $(".dot").position().top,
         });
+    }
+    if(isInView(".inner") && !isAnimated){
+        var listBars = $(".inner");
+        for(var i = 0; i < listBars.length; i++){
+            var percentage = listBars[i].dataset.percentage;
+            $(listBars[i]).text($(listBars[i]).text() + " " + percentage);
+            $(listBars[i]).animate({
+                width: percentage
+            }, 2000);
+        
+        }
+        isAnimated = true;
     }
 });
 
@@ -90,8 +103,10 @@ function closeWork(){
     $(".black-background").fadeOut(function(){
         $(".black-background .text").html(''); //clear for sure (dont have to but... you get the point)
     });
+    
 }
 
 $("#works li").click(function(){
     openWork($(this));
 });
+
